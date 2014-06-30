6d7df315fa7a8c49baa42c2e39bbe041dcdc636f
wangfei0206wl <wf830206>
2014-06-30 15:09:52 +0800

    add opencv demo project

---
+++ OpenCVDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenCVDemo
+//
+//  Created by wangfei on 14-6-30.
+//  Copyright (c) 2014年 wangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
