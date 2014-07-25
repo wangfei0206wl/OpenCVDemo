d9d8f0e4f3a8e139af5f93425c0e7d974af95b2e
wangfei <wangfei0206wl@163.com>
2014-07-25 12:35:38 +0800

    modify contours area demo

--- OpenCVDemo/imagepro/ContoursAreaViewController.h
+++ OpenCVDemo/imagepro/ContoursAreaViewController.h
@@ -19,6 +19,23 @@
  6、计算中心矩
  7、绘制轮廓及中心矩
  8、计算轮廓面积和轮廓周长
+ 
+ 函数说明
+ 1、moments 根据轮廓获取对应的矩
+    原型
+    CV_EXPORTS_W Moments moments( InputArray array, bool binaryImage=false );
+    array: 输入轮廓
+    返回: 矩
+ 2、contourArea 计算轮廓面积
+    原型
+    CV_EXPORTS_W double contourArea( InputArray contour, bool oriented=false );
+    contour: 输入轮廓
+    返回: 轮廓面积
+ 3、arcLength 计算轮廓周长
+    原型
+    CV_EXPORTS_W double arcLength( InputArray curve, bool closed );
+    curve: 输入轮廓
+    返回: 轮廓周长
  */
 @interface ContoursAreaViewController : BaseViewController
 
